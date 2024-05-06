5e5f8e769ae18665ad320e1b26ac6cf117849574
Yash Joshi <yash.joshi@mail.mcgill.ca>
2024-05-05 22:53:18 -0400

    Pushed v1

---
+++ dags/reddit_dag.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
+from pipelines.reddit_pipeline import reddit_pipeline
+
+default_args = {
+    'owner': 'Yash Joshi',
+    'start_date': datetime(2024, 5, 5)
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+dag = DAG(
+    dag_id='redditreviewtracker',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['reddit', 'etl', 'pipeline']
+)
+
+# extraction from reddit
+extract = PythonOperator(
+    task_id='reddit_extraction',
+    python_callable=reddit_pipeline,
+    op_kwargs={
+        'file_name': f'reddit_{file_postfix}',
+        'subreddit': 'shopify',
+        'time_filter': 'day',
+        'limit': 100
+    },
+    dag=dag
+)
+
+# upload to s3
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
+
+extract >> upload_s3
---
+++ pipelines/aws_s3_pipeline.py
@@ -0,0 +1,12 @@
+from etls.aws_etl import connect_to_s3, create_bucket_if_not_exist, upload_to_s3
+from utils.constants import AWS_BUCKET_NAME
+
+
+def upload_s3_pipeline(ti):
+    file_path = ti.xcom_pull(task_ids='reddit_extraction', key='return_value')
+    # Connect to S3
+    s3 = connect_to_s3()
+    # Create bucket
+    create_bucket_if_not_exist(s3, AWS_BUCKET_NAME)
+    # Upload file
+    upload_to_s3(s3, file_path, AWS_BUCKET_NAME, file_path.split('/')[-1])
